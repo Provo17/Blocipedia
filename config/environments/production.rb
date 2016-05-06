09528801a9c08a8ec06d7003130b4dd8087028b2
provo17 <provo17.gp@gmail.com>
2016-05-06 21:56:27 +0000

    mailtrap for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'projects-provo17-1.c9users.io'}  
+  config.action_mailer.default_url_options = { host: 'still-hamlet-95365.herokuapp.com'}  
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
