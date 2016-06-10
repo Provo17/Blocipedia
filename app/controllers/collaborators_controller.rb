ec63de0c6e29ae291b3d0c4720fa364a59c2afb4
provo17 <provo17.gp@gmail.com>
2016-06-10 01:30:56 +0000

    update

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,12 +1,12 @@
 class CollaboratorsController < ApplicationController
-  def remove
+  def delete
         @wiki = Wiki.find(params[:wiki_id])
         collaborator = @wiki.collaborators.find(params[:id])
 
         if @wiki.collaborators.delete(collaborator)
-            flash[:notice] = "Collaborator deleted."
+            flash[:notice] = "Going, going, gone!!!! Collaborator deleted."
         else
-            flash[:alert] = "Deleting collaborator failed."
+            flash[:alert] = "They're still here, action failed."
         end
         redirect_to wikis_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :wikis, only: [] do
     resources :collaborators, only: [] do
       member do
-        put :remove
+        put :delete
       end
     end
   end  
